f5f88f3b2e540491e6176488b91a4343d04d6842
Thomas Fung <thomas.fung1@gmail.com>
2015-05-25 01:02:28 -0600

    added scrolling effect, changed directory structure

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    // internal link scrolling
+    // http://www.paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
